a48e60a6d6f815dd50f0b8cfbea9549cb30df611
Vidski <david.rydwanski@gmail.com>
2023-06-12 22:36:24 +0200

    wip

--- django_tom_select/forms.py
+++ django_tom_select/forms.py
@@ -92,8 +92,8 @@ class TomSelectMixin:
 
     def optgroups(self, name, value, attrs=None):
         """Add empty option for clearable selects."""
-        if not self.is_required and not self.allow_multiple_selected:
-            self.choices = list(chain([(" ", "")], self.choices))
+        # if not self.is_required and not self.allow_multiple_selected:
+        #     self.choices = list(chain([(" ", "")], self.choices))
         return super().optgroups(name, value, attrs)
 
     @property
